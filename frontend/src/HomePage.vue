0fd54fac0277dc5e7e809bb35de9bbac5db6745f
Chris.Vasquez <cvasqu09@gmail.com>
2021-04-28 20:33:45 -0600

    Add linter and start creating contact tree

--- frontend/src/HomePage.vue
+++ frontend/src/HomePage.vue
@@ -1,15 +1,16 @@
 <template>
   <header>
     <h1 class="d-flex">Contact</h1>
+    <ContactTree />
   </header>
 </template>
 
 <script lang="ts">
+import ContactTree from "./components/ContactTree.vue";
 export default {
-  name: "HomePage"
-}
+  name: "HomePage",
+  components: { ContactTree },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/components/ContactTree.vue
+++ frontend/src/components/ContactTree.vue
@@ -1,13 +1,156 @@
 <template>
-$END$
+  <div id="contact-viz" />
 </template>
 
-<script>
+<script lang="ts">
+import * as d3 from "d3";
 export default {
-name: "ContactTree"
-}
+  name: "ContactTree",
+  mounted() {
+    var margin = { top: 10, right: 30, bottom: 30, left: 40 };
+    // append the svg object to the body of the page
+    var svg = d3
+      .select("#contact-viz")
+      .append("svg")
+      .attr("width", 1000)
+      .attr("height", 1000)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const data = {
+      nodes: [
+        {
+          id: 1,
+          name: "A",
+        },
+        {
+          id: 2,
+          name: "B",
+        },
+        {
+          id: 3,
+          name: "C",
+        },
+        {
+          id: 4,
+          name: "D",
+        },
+        {
+          id: 5,
+          name: "E",
+        },
+        {
+          id: 6,
+          name: "F",
+        },
+        {
+          id: 7,
+          name: "G",
+        },
+        {
+          id: 8,
+          name: "H",
+        },
+        {
+          id: 9,
+          name: "I",
+        },
+        {
+          id: 10,
+          name: "J",
+        },
+      ],
+      links: [
+        {
+          source: 1,
+          target: 2,
+        },
+        {
+          source: 1,
+          target: 5,
+        },
+        {
+          source: 1,
+          target: 6,
+        },
+
+        {
+          source: 1,
+          target: 7,
+        },
+        {
+          source: 1,
+          target: 4,
+        },
+      ],
+    };
+
+    // Initialize the links
+    var link = svg
+      .selectAll("line")
+      .data(data.links)
+      .enter()
+      .append("line")
+      .style("stroke", "#aaa");
+
+    // Initialize the nodes
+    var node = svg
+      .selectAll("circle")
+      .data(data.nodes)
+      .enter()
+      .append("circle")
+      .attr("r", 20)
+      .style("fill", "#69b3a2");
+
+    var simulation = d3
+      .forceSimulation(data.nodes) // Force algorithm is applied to data.nodes
+      .force(
+        "link",
+        d3
+          .forceLink() // This force provides links between nodes
+          .id(function (d) {
+            return d.id;
+          }) // This provide  the id of a node
+          .links(data.links)
+          .distance((d) => {
+            console.log("d", d.index);
+            return d.index;
+          })
+      )
+      .force(
+        "collision",
+        d3.forceCollide().radius(() => 20)
+      )
+      .force("charge", d3.forceManyBody().strength(200)) // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+      .force("center", d3.forceCenter(1000 / 2, 1000 / 2)) // This force attracts nodes to the center of the svg area
+      .on("end", ticked);
+
+    // This function is run at each iteration of the force algorithm, updating the nodes position.
+    function ticked() {
+      link
+        .attr("x1", function (d) {
+          return d.source.x;
+        })
+        .attr("y1", function (d) {
+          return d.source.y;
+        })
+        .attr("x2", function (d) {
+          return d.target.x;
+        })
+        .attr("y2", function (d) {
+          return d.target.y;
+        });
+
+      node
+        .attr("cx", function (d) {
+          return d.x + 6;
+        })
+        .attr("cy", function (d) {
+          return d.y - 6;
+        });
+    }
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
